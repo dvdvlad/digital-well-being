899e7fff170ffc767d439b215757c9974f9960d6
Danilov Vlad <vladvd2341@gmail.com>
2023-08-03 10:35:14 +0400

    Database modificated

--- try to make app/Database things/Database.cs
+++ try to make app/Database things/Database.cs
@@ -6,37 +6,63 @@ using System.Windows.Documents;
 namespace try_to_make_app.Database_things;
 
 [Serializable]
-class App: Process
+class App 
 {
-    private DateTime PersonalLimitation = new DateTime(0,0,0,0,0,0);
+    public Process AppProcess;
+    public double WorkToDayTime;
+    public double WorkWeekTime;
+    public App (Process process )
+    {
+        AppProcess = process;
+    }
+
+    public App(Process process, double workToDayTime)
+    {
+        AppProcess = process;
+        WorkToDayTime = workToDayTime;
+    }
 }
 
 [Serializable]
 class Database
 {
-    public List<App> Apps;
+    public List<App> Apps = new List<App>();
     private DateTime LastRun;
+    private List<string> SystemProcess = new List<string>() {"TextInputHost","ApplicationFrameHost","SystemSettings" };
     public List<App> GetApps()
     {
         List<App> apps = new List<App>();
-        Process[] process = App.GetProcesses();
-        foreach (Process pr in process)
+        Process[] processes = Process.GetProcesses();
+        foreach (var pr in processes)
         {
-            foreach (var datapr in Apps)
+
+            if (pr.MainWindowTitle !="")
             {
-                if (datapr.ProcessName == pr.ProcessName)
+                int StateOfChek = 0;
+                foreach (var SysPr in SystemProcess)
                 {
-                    Console.WriteLine("есть в бд");
-                }
-                else
+                    if (pr.ProcessName != SysPr)
+                    {
+                        StateOfChek = 0;
+                    }
+                    else
+                    {
+                        StateOfChek = 1;
+                        break;
+                    }
+                } 
+                if (StateOfChek == 0)
                 {
-                    App app = (App)pr;
-                    
+                    var ProcessWorkToday = Math.Round((DateTime.Now - pr.StartTime).TotalMinutes);
+                    App app = new App(pr,ProcessWorkToday);
+                            
+                            
+                    apps.Add(app);
                 }
             }
         }
+
         return apps;
     }
+    
 }
-
-
