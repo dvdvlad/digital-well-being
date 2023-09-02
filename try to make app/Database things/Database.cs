2e2ef68c0c21eeb8c92453384eb828cc688af397
Danilov Vlad <vladvd2341@gmail.com>
2023-09-02 08:48:40 +0400

    Addd Database class

---
+++ try to make app/Database things/Database.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace try_to_make_app.Database_things;
+
+public class Database
+{
+    private List<AppViewModel> _appViewModels = new List<AppViewModel>();
+
+    public List<AppViewModel> AppViewModels
+    {
+        get
+        {
+            return AppViewModels;
+
+        }
+        set
+        {
+            _appViewModels = value;
+        }
+    }
+
+    private double _weekWorkTime = 0;
+
+    public double WeekWorkTime
+    {
+        get
+        {
+            return _weekWorkTime;
+        }
+        set
+        {
+            _weekWorkTime = value;
+        }
+    }
+
+    private double CountWeekWorkTime()
+    {
+        double MethodWorkTime = 0;
+        foreach (var day in AppViewModels)
+        {
+            foreach (var app in day.Apps)
+            {
+                MethodWorkTime = MethodWorkTime + app.WorkTimeToDay;
+            }
+        }
+
+        return MethodWorkTime;
+    }
+}
